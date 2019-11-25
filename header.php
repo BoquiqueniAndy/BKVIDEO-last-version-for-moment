7371bd9a7d715f1a8bb31b7b7cbbd054a9f4bd02
BoquiqueniAndy <54801230+BoquiqueniAndy@users.noreply.github.com>
2019-11-25 10:17:06 +0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		
+    </head>
+ 
+    <body>
+
+    <nav>  
+		<ul id="nav">
+		
+        <li><a href="../index.php">Accueil</a> </li>
+        <li><a href="actOTD.php?action=inscription">Inscription</a> </li>
+        <li><a href="actOTD.php?action=connexion">Connexion</a> </li>
+        <li><a href="actOTD.php?action=produits">Produits</a> </li>
+        <li><a href="actOTD.php?action=panier">Panier</a> </li>
+	
+		</ul>
+	</nav>
+
+    </body>
+</html>
---
+++ header2.php
@@ -0,0 +1,39 @@
+<nav>
+  <ul>
+    <li class="deroulant"><a href="actOTD.php?action=ps3">PS3 &ensp;</a>
+      <ul class="sous">
+        <li><a href="actOTD.php?action=New1">Nouveautés</a></li>
+        <li><a href="actOTD.php?action=RM1">Récents/Mémorables</a></li>
+        <li><a href="actOTD.php?action=OI1">Offres incroyables</a></li>
+      </ul>
+    </li>
+    <li class="deroulant"><a href="actOTD.php?action=ps4">PS4 &ensp;</a>
+      <ul class="sous">
+        <li><a href="actOTD.php?action=New2">Nouveautés</a></li>
+        <li><a href="actOTD.php?action=RM2">Récents/Mémorables</a></li>
+        <li><a href="actOTD.php?action=OI2">Offres incroyables</a></li>
+      </ul>
+    </li>
+    <li class="deroulant"><a href="actOTD.php?action=pc">PC &ensp;</a>
+      <ul class="sous">
+        <li><a href="actOTD.php?action=New3">Nouveautés</a></li>
+        <li><a href="actOTD.php?action=RM3">Récents/Mémorables</a></li>
+        <li><a href="actOTD.php?action=OI3">Offres incroyables</a></li>
+      </ul>
+    </li>
+    <li class="deroulant"><a href="actOTD.php?action=xboxone">XBOX ONE &ensp;</a>
+      <ul class="sous">
+        <li><a href="actOTD.php?action=New4">Nouveautés</a></li>
+        <li><a href="actOTD.php?action=RM4">Récents/Mémorables</a></li>
+        <li><a href="actOTD.php?action=OI4">Offres incroyables</a></li>
+      </ul>
+    </li>
+    <li class="deroulant"><a href="actOTD.php?action=xbox360">XBOX 360 &ensp;</a>
+      <ul class="sous">
+        <li><a href="actOTD.php?action=New5">Nouveautés</a></li>
+        <li><a href="actOTD.php?action=RM5">Récents/Mémorables</a></li>
+        <li><a href="actOTD.php?action=OI5">Offres incroyables</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
